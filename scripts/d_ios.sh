c2c1289247be3d8b9cb910b994486aae998aa2b1
Ben Awad <benawad97@gmail.com>
2020-12-10 09:37:25 -0600

    fastlane

--- scripts/d_ios.sh
+++ scripts/d_ios.sh
@@ -8,4 +8,4 @@ npx expo build:ios --non-interactive
 
 curl -o app.ipa $(expo url:ipa --non-interactive)
 
-fastlane deliver --verbose --ipa "app.ipa" --skip_screenshots --skip_metadata
+fastlane deliver --verbose --ipa "app.ipa" --skip_screenshots --skip_metadata -u $APPLE_EMAIL
