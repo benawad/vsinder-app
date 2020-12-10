fb4c43aa9ac4d955bb5c0e4ff07a1d26a981513a
Ben Awad <benawad97@gmail.com>
2020-12-10 09:37:30 -0600

    fix theme

--- src/modules/ProfileStack/ChangeTheme.tsx
+++ src/modules/ProfileStack/ChangeTheme.tsx
@@ -1,17 +1,14 @@
 import React, { useContext } from "react";
-import { Text } from "react-native";
-import { FlatList, TouchableOpacity } from "react-native";
+import { FlatList } from "react-native";
 import { ThemeContext } from "../../ThemeProvider";
 import { vscodeThemes } from "../../themes";
 import { Cell } from "../../ui/Cell";
-import { MyButton } from "../../ui/MyButton";
-import { MyHeader } from "../../ui/MyHeader";
-import { MyText } from "../../ui/MyText";
 import { ScreenWrapper } from "../../ui/ScreenWrapper";
 
 interface ChangeThemeProps {}
 
 export const ChangeTheme: React.FC<ChangeThemeProps> = ({}) => {
+  const [, setTheme] = useContext(ThemeContext);
   return (
     <ScreenWrapper noPadding>
       <FlatList
